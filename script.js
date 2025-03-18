e8f9ae67040e5fb88d0708ed365b14a316679670
solen80a <sofialennbom@gmail.com>
2025-03-18 15:26:53 +0100

    Added js to show city name.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const URL = "https://api.openweathermap.org/data/2.5/forecast?lat=59.334591&lon=18.063240&appid=15a1790288c26c9ab80c3b6f2209e071" //Default Stockholm
+
 let lat = []
 let lon = []
 let fetchedData = []
+let cityName = document.getElementById("cityName")
 
 // console.log(URL)
 
@@ -20,12 +22,19 @@ const fetchData = async () => {
     console.log(fetchedData)
 
         
+    updateCity();
+  } catch (error) {
+    alert("There was an error, please try again later: " + error);
+    console.error("Error fetching data:", error);
   }
-  catch (error){
-    alert("There was an error, please try agein later" + error)
-    console.error("Error fetching data:", error)
-  }
-
 }
+
+const updateCity = () => {
+  if (fetchedData.city && fetchedData.city.name) {
+    cityName.innerHTML = fetchedData.city.name;
+  } else {
+    console.error("City data is missing in fetchedData", fetchedData);
+  }
+};
 
 fetchData()
